7fc43bc6df93f1c76b5d16863dc687844795a575
Alisson <135671808+al1ss0@users.noreply.github.com>
2025-06-26 14:20:42 -0300

    Produtos do Petunia

--- product-service/src/main/resources/db_migration/V2__PopulateTableTbProduct.sql
+++ product-service/src/main/resources/db_migration/V2__PopulateTableTbProduct.sql
@@ -1,5 +1,27 @@
 INSERT INTO tb_product (description, brand, model, currency, price, stock) VALUES
-  ('Iphone 15 256', 'Apple', 'iPhone 15 256GB', 'USD', 1022.96, 10),
-  ('Moto G85 256', 'Motorola', 'G85 256GB', 'USD', 306.78, 10),
-  ('Redmi 13c 256', 'Xiaomi', 'Redmi 13C 256GB', 'USD', 242.15, 10),
-  ('S23 Ultra 256', 'Samsung', 'S23 Ultra 256GB', 'USD', 768.70, 10);
+-- Parafusos
+('Parafuso Bolts', 'Bolts', 'Paraf. Bolts', 'BRL', 24.90, 10),
+('Parafuso Ace', 'Ace', 'Paraf. Ace', 'BRL', 39.90, 10),
+('Parafuso Independent', 'Independent', 'Paraf. Indep.', 'BRL', 39.90, 10),
+
+-- Rodas
+('Roda OJ', 'OJ', 'Rodas OJ', 'BRL', 239.90, 10),
+('Roda Screen', 'Screen', 'Rodas Screen', 'BRL', 120.00, 10),
+('Roda Chocolate', 'Chocolate', 'Rodas Choc.', 'BRL', 249.90, 10),
+('Roda White', 'White', 'Rodas White', 'BRL', 249.90, 10),
+('Roda Girl', 'Girl', 'Rodas Girl', 'BRL', 149.90, 10),
+('Roda Eyes', 'Eyes', 'Rodas Eyes', 'BRL', 149.90, 10),
+
+-- Rolamentos
+('Rolamento Bronson', 'Bronson', 'Rolamento Bronson', 'BRL', 230.00, 10),
+('Rolamento The Girl', 'The Girl', 'Rolamento The Girl', 'BRL', 159.90, 10),
+('Rolamento Jaws', 'Jaws', 'Rolamento Jaws', 'BRL', 379.90, 10),
+('Rolamento Gravete', 'Gravete', 'Rolamento Gravete', 'BRL', 379.90, 10),
+('Rolamento Reds Black', 'Reds', 'Rolamento Reds Black', 'BRL', 279.90, 10),
+('Rolamento Reds', 'Reds', 'Rolamento Reds', 'BRL', 279.90, 10),
+
+-- Shapes
+('Shape Two Face', 'Two Face', 'Shape Two Face', 'BRL', 359.90, 10),
+('Shape Caravana', 'Caravana', 'Shape Caravana', 'BRL', 359.90, 10),
+('Shape Thank You', 'Thank You', 'Shape Thank You', 'BRL', 349.90, 10),
+('Shape Congrats', 'Congrats', 'Shape Congrats', 'BRL', 349.90, 10);
--- product-service/src/main/resources/db_migration/V3__AlterAndUpdateTableTbUser.sql
+++ product-service/src/main/resources/db_migration/V3__AlterAndUpdateTableTbUser.sql
@@ -3,7 +3,30 @@ ALTER TABLE tb_product
 ADD COLUMN image_url VARCHAR(255);
 
 -- Atualiza os registros existentes
-UPDATE tb_product SET image_url = 'https://example.com/images/iphone15.jpg' WHERE model = 'iPhone 15 256GB';
-UPDATE tb_product SET image_url = 'https://example.com/images/motog85.jpg' WHERE model = 'G85 256GB';
-UPDATE tb_product SET image_url = 'https://example.com/images/redmi13c.jpg' WHERE model = 'Redmi 13C 256GB';
-UPDATE tb_product SET image_url = 'https://example.com/images/s23ultra.jpg' WHERE model = 'S23 Ultra 256GB';
+-- Parafusos
+UPDATE tb_product SET image_url = 'https://example.com/images/parafuso-bolts.jpg' WHERE model = 'Paraf. Bolts';
+UPDATE tb_product SET image_url = 'https://example.com/images/parafuso-ace.jpg' WHERE model = 'Paraf. Ace';
+UPDATE tb_product SET image_url = 'https://example.com/images/parafuso-independent.jpg' WHERE model = 'Paraf. Indep.';
+
+-- Rodas
+UPDATE tb_product SET image_url = 'https://example.com/images/roda-oj.jpg' WHERE model = 'Rodas OJ';
+UPDATE tb_product SET image_url = 'https://example.com/images/roda-screen.jpg' WHERE model = 'Rodas Screen';
+UPDATE tb_product SET image_url = 'https://example.com/images/roda-chocolate.jpg' WHERE model = 'Rodas Choc.';
+UPDATE tb_product SET image_url = 'https://example.com/images/roda-white.jpg' WHERE model = 'Rodas White';
+UPDATE tb_product SET image_url = 'https://example.com/images/roda-girl.jpg' WHERE model = 'Rodas Girl';
+UPDATE tb_product SET image_url = 'https://example.com/images/roda-eyes.jpg' WHERE model = 'Rodas Eyes';
+
+-- Rolamentos
+UPDATE tb_product SET image_url = 'https://example.com/images/rolamento-bronson.jpg' WHERE model = 'Rolamento Bronson';
+UPDATE tb_product SET image_url = 'https://example.com/images/rolamento-the-girl.jpg' WHERE model = 'Rolamento The Girl';
+UPDATE tb_product SET image_url = 'https://example.com/images/rolamento-jaws.jpg' WHERE model = 'Rolamento Jaws';
+UPDATE tb_product SET image_url = 'https://example.com/images/rolamento-gravete.jpg' WHERE model = 'Rolamento Gravete';
+UPDATE tb_product SET image_url = 'https://example.com/images/rolamento-reds-black.jpg' WHERE model = 'Rolamento Reds Black';
+UPDATE tb_product SET image_url = 'https://example.com/images/rolamento-reds.jpg' WHERE model = 'Rolamento Reds';
+
+-- Shapes
+UPDATE tb_product SET image_url = 'https://example.com/images/shape-two-face.jpg' WHERE model = 'Shape Two Face';
+UPDATE tb_product SET image_url = 'https://example.com/images/shape-caravana.jpg' WHERE model = 'Shape Caravana';
+UPDATE tb_product SET image_url = 'https://example.com/images/shape-thank-you.jpg' WHERE model = 'Shape Thank You';
+UPDATE tb_product SET image_url = 'https://example.com/images/shape-congrats.jpg' WHERE model = 'Shape Congrats';
+
